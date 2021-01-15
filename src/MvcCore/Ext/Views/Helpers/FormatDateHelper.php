3b795e2832ce537a24d31ce43bf818c3e37d9dd8
Tom Flídr <tomflidr@gmail.com>
2021-01-15 22:46:20 +0100

    Fixed copyright heading, fixed php docs comments with unnecessary interfaces, upgraded version constant to 5.0.0

--- src/MvcCore/Ext/Views/Helpers/FormatDateHelper.php
+++ src/MvcCore/Ext/Views/Helpers/FormatDateHelper.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view
  * the LICENSE.md file that are distributed with this source code.
  *
- * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @copyright	Copyright (c) 2016 Tom Flidr (https://github.com/mvccore)
  * @license		https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
  */
 
@@ -21,14 +21,14 @@ namespace MvcCore\Ext\Views\Helpers;
  * - Fallback result string always returned in response encoding, in UTF-8 by default.
  * @method \MvcCore\Ext\Views\Helpers\FormatDateHelper GetInstance()
  */
-class FormatDateHelper extends \MvcCore\Ext\Views\Helpers\InternationalizedHelper
-{
+class FormatDateHelper extends \MvcCore\Ext\Views\Helpers\InternationalizedHelper {
+
 	/**
 	 * MvcCore Extension - View Helper - Assets - version:
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.0-alpha';
+	const VERSION = '5.0.0';
 
 	/**
 	 * If this static property is set - helper is possible
